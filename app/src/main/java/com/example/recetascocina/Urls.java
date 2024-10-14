3097899c9d1f4514bce458fb1457d95a689dbc43
Cruz Carlos <ccruzworks@gmail.com>
2024-10-13 23:22:15 -0400

    agregacion de enlaces al server

---
+++ app/src/main/java/com/example/recetascocina/Urls.java
@@ -0,0 +1,14 @@
+package com.example.recetascocina;
+
+public class Urls {
+    public static final String BASE_URL = "http://...:80/recetas/";
+    public static final String REMOTO_BUSQUEDA_URL = BASE_URL + "remoto_busqueda.php";
+    public static final String REMOTO_CREAR_RECETA_URL = BASE_URL + "remoto_crearReceta.php";
+    public static final String REMOTO_FAVORITOS_URL = BASE_URL + "remoto_favoritos.php";
+    public static final String REMOTO_HISTORIAL_URL = BASE_URL + "remoto_historial.php";
+    public static final String REMOTO_LIMPIAR_HISTORIAL_URL = BASE_URL + "remoto_limpiarHistorial.php";
+    public static final String REMOTO_MAS_VISITADOS_URL = BASE_URL + "remoto_masVisitados.php";
+    public static final String REMOTO_MIS_RECETAS_URL = BASE_URL + "remoto_misRecetas.php";
+    public static final String REMOTO_VERIFICAR_INICIO_URL = BASE_URL + "remoto_verificarInicio.php";
+    public static final String REMOTO_VER_RECETA_URL = BASE_URL + "remoto_verReceta.php";
+}
