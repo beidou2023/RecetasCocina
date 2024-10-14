db3287de5707f9f1d6d17fb357f2c7b405209972
Cruz Carlos <ccruzworks@gmail.com>
2024-10-14 10:21:44 -0400

    busqueda filtro

--- app/src/main/java/com/example/recetascocina/Busqueda.java
+++ app/src/main/java/com/example/recetascocina/Busqueda.java
@@ -1,6 +1,9 @@
 package com.example.recetascocina;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,6 +13,7 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class Busqueda extends AppCompatActivity {
 
+    EditText agregar,quitar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +24,21 @@ public class Busqueda extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        agregar=findViewById(R.id.ett_agregarIng);
+        quitar=findViewById(R.id.ett_quitarIng);
+    }
+
+    public void irHome(View v){
+        Intent it=new Intent(getApplicationContext(), Home.class);
+        startActivity(it);
+    }
+
+    public void mostrarFiltro(View v){
+        Intent it=new Intent(getApplicationContext(), BusquedaRes.class);
+        String agregarR=agregar.getText().toString();
+        String quitarR=quitar.getText().toString();
+        it.putExtra("agrearIngrediente",agregarR);
+        it.putExtra("quitarIngrediente",quitarR);
+        startActivity(it);
     }
 }
